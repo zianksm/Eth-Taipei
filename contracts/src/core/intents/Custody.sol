80a7e4aa2d66e02c83e5e7d31cc846e5003cc875
zianksm <mynameiszian@gmail.com>
2025-04-04 22:43:15 +0800

    feat: added simple token action abstraction

--- contracts/src/core/intents/Custody.sol
+++ contracts/src/core/intents/Custody.sol
@@ -1,14 +1,11 @@
 pragma solidity ^0.8.0;
 
 import {IntentLibrary} from "./../../libraries/Intent.sol";
-import {SafeERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
-import {IERC20} from "openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+import {TokenAction} from "./../TokenAction.sol";
 
 // needed to lock user funds for the intent, because if not then there's a possibility the intent fails and create a DOS situation
 // where intent keeps failing, so user funds needs to be locked here
-contract FundsCustody {
-    using SafeERC20 for IERC20;
-
+contract FundsCustody is TokenAction {
     // user => token => locked balance
     // this is used for locking user funds, if a user makes an intent, the funds will go here
     mapping(address => mapping(address => uint256)) public locked;
@@ -23,7 +20,7 @@ contract FundsCustody {
 
         locked[msg.sender][token] -= amount;
 
-        IERC20(token).safeTransfer(msg.sender, amount);
+        transfer(token, msg.sender, amount);
     }
 
     function withdrawFree(address token, uint256 amount) external {
@@ -32,6 +29,6 @@ contract FundsCustody {
 
         free[msg.sender][token] -= amount;
 
-        IERC20(token).safeTransfer(msg.sender, amount);
+        transfer(token, msg.sender, amount);
     }
 }
